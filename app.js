7cb9b8dcdb52c85cff5b8d16cec4119f8c65c755
Uday Krishna <uday.krishna10000@gmail.com>
2020-12-12 08:42:23 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var username = prompt('Give me your username');
-alert("this script works " + username);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput)
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler);
