7095b9bd0c64c3445ce6b9bb5d03d4ceb0d4d978
Uday Krishna <uday.krishna10000@gmail.com>
2020-12-13 15:06:43 +0530

    translation done

--- app.js
+++ app.js
@@ -2,10 +2,32 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl() {
+    return serverUrl + "?" + "text=" + txtInput.value
+}
+
+function errorHandler(error){
+    console.log("error occured" + error);
+    alert("There is a problem with the server. Try again later");
+}
+
 
 function clickHandler() {
+    
+    var inputText = txtInput.value;
 
-    outputDiv.innerText = "asdjkha " + txtInput.value;
-};
+    // outputDiv.innerText = "asdjkha " + txtInput.value;
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+    };
 
 btnTranslate.addEventListener("click", clickHandler)
